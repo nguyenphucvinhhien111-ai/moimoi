caa60a454c1396ffeceb22cdf5d5a782a067dfd1
vinhhien <nguyenphucvinhhien111@gmail.com>
2025-10-04 10:15:22 +0700

    da xong

--- src/socket/socket.java
+++ src/socket/socket.java
@@ -1,9 +1,25 @@
 package socket;
-
+ 
+import java.io.IOException;
+import java.net.URL;
+ 
 public class socket {
-
-	public static void main(String[] args) {
-			System.out.println("Hello world!");
-	}
-
+ 
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.gpcoder.com:80/java/index.html?page=1&amp;amp;amp;order=desc#java-core");
+            System.out.println("URL : " + url.toString());
+            System.out.println("protocol : " + url.getProtocol());
+            System.out.println("authority : " + url.getAuthority());
+            System.out.println("file name : " + url.getFile());
+            System.out.println("host : " + url.getHost());
+            System.out.println("path : " + url.getPath());
+            System.out.println("port : " + url.getPort());
+            System.out.println("default port : " + url.getDefaultPort());
+            System.out.println("query : " + url.getQuery());
+            System.out.println("ref : " + url.getRef());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
